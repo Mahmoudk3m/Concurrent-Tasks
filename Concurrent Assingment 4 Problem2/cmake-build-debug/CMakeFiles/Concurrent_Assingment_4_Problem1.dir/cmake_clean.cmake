5c578cf4fcb6dfc3273c289c16530d7e734fcbdb
MahmoudK3m <mahmoudk3m.mk@gmail.com>
2022-12-24 19:54:37 +0200

    added assingment 4

---
+++ Concurrent Assingment 4 Problem2/cmake-build-debug/CMakeFiles/Concurrent_Assingment_4_Problem1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Concurrent_Assingment_4_Problem1.dir/main.cpp.obj"
+  "Concurrent_Assingment_4_Problem1.exe"
+  "Concurrent_Assingment_4_Problem1.exe.manifest"
+  "Concurrent_Assingment_4_Problem1.pdb"
+  "libConcurrent_Assingment_4_Problem1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Concurrent_Assingment_4_Problem1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Concurrent Assingment 4 Problem2/main.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+//ACPC2022
+//TposersFTW
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pf push_front
+#define lcm(x,y) x*y/__gcd(x,y)
+#define summ(n) (n*(n+1))/2
+#define loop(x,n) for(int i=x;i<n;i++)
+#define loopj(x,n) for(int j=x;j<n;j++)
+#define int long long
+#define test int t;cin>>t;while(t--)
+using namespace std;
+double eps = 1e-9;
+int const n=4,m=4;
+int x=49;
+int mesh[n][m];
+int search(int r,int c){
+    if(r==n||c==m)return mesh[0][0];
+    if(mesh[r][c]<mesh[0][0]){
+        swap(mesh[0][0],mesh[r][c]);
+    }
+    search(r+1,c);
+    search(r,c+1);
+}
+int32_t main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cin.tie(0);std::ios::sync_with_stdio(false);cout.tie(0);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>mesh[i][j];
+        }
+    }
+    cout<<search(0,0);
+    return 0;
+}
